66ff3c14d6d7a40b0118b2f93923e377db9b532f
LouDnl <thierry@smeekes.nl>
2015-01-24 13:58:18 +0100

    Change chopshop and changeownership

--- server/CfgFunctions.hpp
+++ server/CfgFunctions.hpp
@@ -7,6 +7,8 @@
 class ServerDir
 {
 	class generateKey {file = "server\functions\network\generateKey.sqf";};
+	class setupOwnerTruck {file = "server\functions\setupOwnerTruck.sqf";};
+	class setupSellTruck {file = "server\functions\setupSellTruck.sqf";};
 	class setupStoreNPC {file = "server\functions\setupStoreNPC.sqf";};
 	class setupMissionObject {file = "server\functions\setupMissionObject.sqf";};
 	class setupMissionATM {file = "server\functions\setupMissionATM.sqf";};
