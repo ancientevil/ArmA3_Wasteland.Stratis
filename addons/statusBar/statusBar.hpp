78c51bf7f500f57337fa236fa135b29cc98fb6cb
Ancientevil <lordmaju@hotmail.com>
2015-05-25 18:09:14 +0200

    A better looking statusbar

--- addons/statusBar/statusBar.hpp
+++ addons/statusBar/statusBar.hpp
@@ -1,37 +1,4 @@
 #define ST_RIGHT 0x01
- 
-class osefStatusBarAdmin {
-	idd = -1;
-	onLoad = "uiNamespace setVariable ['osefStatusBarAdmin', _this select 0]";
-	onUnload = "uiNamespace setVariable ['osefStatusBarAdmin', objNull]";
-	onDestroy = "uiNamespace setVariable ['osefStatusBarAdmin', objNull]";
-	fadein = 0;
-	fadeout = 0;
-	duration = 10e10;
-	movingEnable = 0;
-	controlsBackground[] = {};
-	objects[] = {};
-	class controls {
-		class statusBarText {
-			idc = 55554;
-			x = safezoneX + safezoneW - 1.90;
-			y = safezoneY + safezoneH - 0.063;
-			w = 1.35;
-			h = 0.06;
-			shadow = 2;
-			//colorBackground[] = { 0, 0, 0, 0.5 };  // uncomment and increase 4th number to have a background
-			font = "PuristaSemibold";
-			size = 0.04;
-			type = 13;
-			style = 2;
-			text="";
-			class Attributes {
-				align="center";
-				color = "#ffffff";//#5fe60c
-			};
-		};
-	};
-}; 
 
 class osefStatusBar {
 	idd = -1;
@@ -41,27 +8,27 @@ class osefStatusBar {
 	fadein = 0;
 	fadeout = 0;
 	duration = 10e10;
-	movingEnable = 0;
+	movingEnable = 1;
 	controlsBackground[] = {};
 	objects[] = {};
 	class controls {
 		class statusBarText {
-			idc = 55555;
-			x = safezoneX + safezoneW - 1.50;
-			y = safezoneY + safezoneH - 0.035;
-			w = 1.3;
-			h = 0.07;
+			idc = 1000;
+			x = safezoneX + safezoneW - 1;
+			y = safezoneY + safezoneH - 0.045;
+			w = 0.92;
+			h = 0.04;
 			shadow = 2;
-			colorBackground[] = { 0, 0, 0, 0.5 };  // uncomment and increase 4th number to have a background
+			colorBackground[] = { 1, 0.3, 0, 0 };  // uncomment and increase 4th number to have a background
 			font = "PuristaSemibold";
 			size = 0.04;
 			type = 13;
-			style = 2;
-			text="";
+			style = 1;
+			text="Loading ...";
 			class Attributes {
 				align="left";
-				color = "#ffffff";//#5fe60c
+				color = "#7E7E7F";
 			};
 		};
 	};
-}; 
+};
